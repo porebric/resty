471b362450cc585137b1be20d68b5a3aa42b4afc
porebric <vladimir.yalymov@yandex.ru>
2024-01-03 14:00:01 +0300

    set not valid field message

--- middleware/request_validate.go
+++ middleware/request_validate.go
@@ -13,15 +13,18 @@ type RequestValidate struct {
 }
 
 func (r *RequestValidate) Execute(req requests.Request) (int32, string) {
-	valid, field := req.Validate()
+	valid, field, msg := req.Validate()
 	if valid {
 		return r.next.Execute(req)
 	}
 	if field == "" {
 		return errors.ErrorInvalidRequest, ""
-	} else {
-		return errors.ErrorCustomError, fmt.Sprintf("field %s is missing", field)
 	}
+
+	if msg == "" {
+		msg = "invalid"
+	}
+	return errors.ErrorInvalidRequest, fmt.Sprintf("field %s: %s", field, msg)
 }
 
 func (r *RequestValidate) SetNext(next Middleware) {
--- requests/request.go
+++ requests/request.go
@@ -3,7 +3,7 @@ package requests
 import "net/http"
 
 type Request interface {
-	Validate() (bool, string)
+	Validate() (bool, string, string)
 	Set(r *http.Request) error
 	Middlewares() map[string]bool
 
